9e0f691068cb21cf5a3fdc69b864f7576266a056
Alvader01 <alfersanchez05@gmail.com>
2024-02-06 11:25:11 +0100

    Library

--- Model/Repo/Library.java
+++ Model/Repo/Library.java
@@ -1,4 +1,25 @@
 package Model.Repo;
 
-public class Library {
+import Interfaces.IRepo;
+import Model.Entity.Favourite;
+
+public class Library implements IRepo {
+    private Favourite[] favoritos = new Favourite[10];
+
+
+    @Override
+    public Favourite[] muestraFavoritos() {
+        return new Favourite[0];
+    }
+
+    @Override
+    public boolean anadirFavorito(Favourite f) {
+        return false;
+    }
+
+    @Override
+    public Favourite quitarFavorito(String n) {
+        return null;
+    }
 }
+
