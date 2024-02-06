658413a08119e404760a15754a7c6cfd99a0fcaf
Alvader01 <alfersanchez05@gmail.com>
2024-02-06 11:17:05 +0100

    modelos completos menos library

--- Model/Entity/Movie.java
+++ Model/Entity/Movie.java
@@ -2,4 +2,29 @@ package Model.Entity;
 
 public class Movie extends Favourite {
     private int minutos;
+
+    public Movie(String nombre, String pais, int ano, int minutos) {
+        super(nombre, pais, ano);
+        this.minutos = minutos;
+    }
+
+    public Movie(int minutos) {
+        super();
+        minutos = 0;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+    @Override
+    public String toString() {
+        return "Minutos [minutos=" + minutos + "]"+super.toString();
+    }
+    public Movie(String nombre, String pais, int ano){
+        super(nombre, pais,ano);
+    }
 }
