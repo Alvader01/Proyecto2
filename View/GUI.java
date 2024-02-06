8ddba9eed7794db84216bc7eea931f70f7e1ae6d
Alfonso <alfonsojaen200533@gmail.com>
2024-02-06 10:10:16 +0100

    Actualizando

--- View/GUI.java
+++ View/GUI.java
@@ -1,4 +1,9 @@
 package View;
 
 public class GUI {
+    public void muestraMenuPrincipal() {
+        System.out.println("1. Agregar");
+        System.out.println("2. Mostrar");
+        System.out.println("3. Salir");
+    }
 }
